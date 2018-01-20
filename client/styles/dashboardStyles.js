79b3362a1b62b3c695c2cc8ff712b0674471dd54
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-01-20 00:28:11 -1000

    mobile styling dashboard...lol

--- client/styles/dashboardStyles.js
+++ client/styles/dashboardStyles.js
@@ -2,94 +2,111 @@ import css from 'styled-jsx/css'
 
 export default css`
     
+.dashboard-container {
+    background-color: #1a2930;
+    padding: 40px;
+    height: 100vh;
+}
+
+.client-header {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+}
+
+.breadcrumb {
+    padding: 0;
+    margin: 0;
+    /*border-bottom: 1.5px solid #f5f5f5;*/
+    background-color: #ffffff; 
+    border-bottom: 1.5px solid #f5f5f5;
+    margin-bottom: 20px;
+}
+
+.breadcrumb-item {
+    color: #999999;
+    padding: 15px 0 15px 0;
+    border-bottom: 1.5px solid #f5f5f5;
+}
+
+.breadcrumb-item:hover {
+    cursor: pointer;
+}
+
+.is-active {
+    color: #2c4551;
+    border-color: #2c4551;
+}
+
+.not-active:hover{
+    color: #777777;
+}
+
+.table-container {
+    background-color: #ffffff;
+    padding: 40px;
+    height: 100%;
+}
+
+table {
+    border: 1.5px solid #f5f5f5;
+    border-radius: 3px;
+    line-height: normal;
+}
+
+th {
+    background-color: #f5f5f5;
+    padding: 10px;
+}
+
+td {
+    padding: 10px;
+        border-top: 1.5px solid #f5f5f5;
+    }
+    
+.client-table {
+    background-color: #ffffff;
+    color: #28434f;
+    width: 100%;
+}
+
+.client-name {
+    color: #335260;
+    font-weight: bold;
+}
+
+.client-name:hover {
+    cursor: pointer;
+}
+
+.last-table-column {
+    width: 13%;    
+}
+
+.hidden-x {
+    opacity: 0;
+}
+
+.hidden-x:hover {
+    opacity: .75;
+    color: #ff2b2b;
+    cursor: pointer;
+}
+
+@media screen and (max-width: 425px) {
+
     .dashboard-container {
-        background-color: #1a2930;
-        padding: 40px;
+        padding: 20px;
     }
-
-    .client-header {
-        display: flex;
-        justify-content: space-between;
-        flex-direction: row;
-    }
-
-    .breadcrumb {
-        padding: 0;
-        margin: 0;
-        /*border-bottom: 1.5px solid #f5f5f5;*/
-        background-color: #ffffff; 
-        border-bottom: 1.5px solid #f5f5f5;
-        margin-bottom: 20px;
-    }
-
-    .breadcrumb-item {
-        color: #999999;
-        padding: 15px 0 15px 0;
-        border-bottom: 1.5px solid #f5f5f5;
-    }
-
-    .breadcrumb-item:hover {
-        cursor: pointer;
-    }
-
-    .is-active {
-        color: #2c4551;
-        border-color: #2c4551;
-    }
-
-    .not-active:hover{
-        color: #777777;
-    }
-
-    .table-container {
-        background-color: #ffffff;
-        padding: 40px;
-        height: 100%;
-    }
-    
-    table {
-        border: 1.5px solid #f5f5f5;
-        border-radius: 3px;
-        line-height: normal;
-    }
-    
-    th {
-        background-color: #f5f5f5;
-        padding: 10px;
-    }
-
-    td {
-        padding: 10px;
-            border-top: 1.5px solid #f5f5f5;
-        }
         
-    .client-table {
-        background-color: #ffffff;
-        color: #28434f;
-        width: 100%;
+    .table-container {
+        padding: 20px;
     }
 
-    .client-name {
-        color: #335260;
-        font-weight: bold;
+    td:nth-child(3), th:nth-child(3) {
+        display: none;
     }
+}
 
-    .client-name:hover {
-        cursor: pointer;
-    }
-
-    .last-table-column {
-        width: 13%;    
-    }
-
-    .hidden-x {
-        opacity: 0;
-    }
-
-    .hidden-x:hover {
-        opacity: .75;
-        color: #ff2b2b;
-        cursor: pointer;
-    }
 
 `
