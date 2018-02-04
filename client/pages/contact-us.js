9c5c6f9701afaed6c642dce0461927bd31687d08
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-02-03 16:16:58 -1000

    working on contact page

--- client/pages/contact-us.js
+++ client/pages/contact-us.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout.js';
+import contactUsStyles from '../styles/contactUsStyles.js';
 import {Container, Row, Col} from 'reactstrap';
 
 class ContactUs extends Component {
@@ -12,9 +13,15 @@ class ContactUs extends Component {
     render(){
         return (
             <Layout>
-                <div>
+                <div className="contact-us-splash">
+                    <h1>CONTACT US</h1>
+                    <p>Please contact us for all of your bail needs</p>  
+                    <p>We are eager to answer and assist you in the best way possible</p>
+                </div>
+                <div className="contact-us-container">
                     This is the contact us page
                 </div>
+                <style jsx>{contactUsStyles}</style>
             </Layout>
         )
     }
---
+++ client/styles/contactUsStyles.js
@@ -0,0 +1,27 @@
+import css from 'styled-jsx/css'
+
+export default css`
+
+    h1 {
+        margin-bottom: 20px;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    .contact-us-splash {
+        background-color: #1a2930;
+        text-align: center;
+        height: 532px;
+        color: #ffffff;
+        padding: 150px 20px 0px 20px;
+    }
+    
+    .contact-us-container {
+        min-height: 100vh;
+        max-width: 1100px;
+        margin: auto;
+        padding: 40px;
+    }
+`
